7ef72e47ed92a4dd2cfc82e9504279e177178e22
peta <doubravova.petramarie@gmail.com>
2022-12-26 22:32:43 +0100

    a jeste jedna oprava cesty

--- info-nas-training/pretrain.sh
+++ info-nas-training/pretrain.sh
@@ -6,7 +6,7 @@ datename=$(date +%m%d-%H%M)
 
 DATADIR=/storage/praha1/home/ayshi/coding
 infodir="$DATADIR"/results
-codedir="$DATADIR"/overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population
+codedir="$DATADIR"/odPremka/PPO/overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population
 
 echo "$datename $PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $infodir/jobs_info.txt
 
@@ -15,7 +15,7 @@ bash $DATADIR/pbs-tools/setup_venv.sh || { echo 'Venv creation failed'; exit1 ; 
 
 
 
-python "$codedir"/odPremka/PPO/diverse_pool_build.py > "$SCRATCHDIR"/out.txt 2> "$SCRATCHDIR"/err.txt
+python "$codedir"/diverse_pool_build.py > "$SCRATCHDIR"/out.txt 2> "$SCRATCHDIR"/err.txt
 
 cp "$SCRATCHDIR"/out.txt "$infodir"/"$datename"_out.txt
 cp "$SCRATCHDIR"/err.txt "$infodir"/"$datename"_err.txt
