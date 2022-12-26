48c7a9fe183e0f1d30acb3807e9886daa47af1e3
peta <doubravova.petramarie@gmail.com>
2022-12-26 22:27:07 +0100

    oprava cesty

--- info-nas-training/pretrain.sh
+++ info-nas-training/pretrain.sh
@@ -4,11 +4,11 @@
 #test -n "$hashno" || { echo >&2 "Variable hashno is not set!"; exit 1; }
 datename=$(date +%m%d-%H%M)
 
-DATADIR=/storage/praha1/home/ayshi/coding/
+DATADIR=/storage/praha1/home/ayshi/coding
 infodir="$DATADIR"/results
 codedir="$DATADIR"/overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population
 
-echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $infodir/jobs_info.txt
+echo "$datename $PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $infodir/jobs_info.txt
 
 bash $DATADIR/pbs-tools/setup_venv.sh || { echo 'Venv creation failed'; exit1 ; }
 . "$SCRATCHDIR"/pyt/bin/activate
